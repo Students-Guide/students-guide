f19c4835a5e52ef6b02e08bad9d2ecd95774e183
jouinibilel <bileljouinipro@hotmail.com>
2021-12-21 15:44:16 +0100

    pulling the server

--- students-guide/src/main.js
+++ students-guide/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// import post from './components/teacherPost.vue'
+// components/teacherPost.vue
 new Vue({
   el: '#app',
   render: h => h(App)
